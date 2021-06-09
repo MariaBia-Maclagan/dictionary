89cf6141d310dd2e42d5899954a1b0d8a5b1f85a
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2021-06-09 19:07:07 +0100

    Created react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="Container">
+      <header className="App-header">
+        <p>Hello World</p>
+      </header>
+    </div>
+  );
+}
+
