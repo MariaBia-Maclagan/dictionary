47b739db55a8d9815dc52b9765b139d5f378b817
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2021-06-19 13:52:05 +0100

    implemented photos component

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 import React, {useState} from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Search.css";
 
 export default function Search(props){
     const[keyWord, setKeyWord] = useState (props.defaultKeyWord);
     const[results, setResults] = useState(null);
-    const [loaded, setLoaded] =useState(false)
+    const [loaded, setLoaded] =useState(false);
+    const[photos, setPhotos] =useState();
+
+function handlePexels (response){
+    setPhotos (response.data.photos);
+}
 
     function handleResponse(response){
         setResults(response.data[0]);
@@ -14,6 +20,10 @@ export default function Search(props){
     function Search (){
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let apiKeyPexels = `563492ad6f91700001000001addac54c34fd4d02bb849da638fc7ce0`;
+        let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyWord}&per_page=6`;
+        axios.get(pexelsUrl, {headers:{"Authorization" : `Bearer ${apiKeyPexels}`},}).then(handlePexels);
     }
 
     function handleSearch(event){
@@ -38,6 +48,7 @@ if(loaded){
             </form>
             </section>
             <Results results={results}/>
+            <Photos photos={photos}/>
         </div>
     );
 }else{
