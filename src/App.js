7118d99348a259afe56f8f701b55867c8a05afeb
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2021-06-21 12:00:43 +0100

    added linked in link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ export default function App() {
         < Search defaultKeyWord="Creativity"/>
       </header>
       <footer> 
-        <a href="https://github.com/MariaBia-Maclagan/dictionary" target="_blank" rel="noreferrer">Open sorce code </a> by M F Maclagan </footer>
+        <a href="https://github.com/MariaBia-Maclagan/dictionary" target="_blank" rel="noreferrer">Open sorce code </a> by M F Maclagan
+        <a href="https://www.linkedin.com/in/maria-beatriz-m-f-maclagan-58376813/" target="_blank" rel="noreferrer"> <i class="fab fa-linkedin"></i> </a> </footer>
     </div>
   );
 }
